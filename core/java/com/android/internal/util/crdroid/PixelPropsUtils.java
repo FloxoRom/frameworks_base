ca98a75ac828c9171e3ead79454391495b13d24f
Cugry <knownerx@gmail.com>
2022-08-07 23:56:42 +0900

    Revert "PixelPropsUtils: Consolidate google app checks"
    This reverts commit 2103de257f900774ea61394c6d0a9e4559d765ba.

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -100,6 +100,9 @@ public class PixelPropsUtils {
         if (packageName == null) {
             return;
         }
+        if (packageName.equals(PACKAGE_GMS)) {
+            sIsGms = true;
+        }
         if (packageName.startsWith("com.google.") || Arrays.asList(extraPackagesToChange).contains(packageName)) {
             Map<String, Object> propsToChange = propsToChangePixel6;
 
@@ -122,13 +125,10 @@ public class PixelPropsUtils {
                 if (DEBUG) Log.d(TAG, "Defining " + key + " prop for: " + packageName);
                 setPropValue(key, value);
             }
-            if (packageName.equals(PACKAGE_GMS)) {
-                sIsGms = true;
-            }
-            // Set proper indexing fingerprint
-            if (packageName.equals("com.google.android.settings.intelligence")) {
-                setPropValue("FINGERPRINT", Build.DATE);
-            }
+        }
+        // Set proper indexing fingerprint
+        if (packageName.equals("com.google.android.settings.intelligence")) {
+            setPropValue("FINGERPRINT", Build.DATE);
         }
     }
 
